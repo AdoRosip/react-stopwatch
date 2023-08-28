ceafcad0772afbb314e8401470d540da3cc88ea0
Adrian Rosipal <adrian.rosipal98@gmail.com>
2023-08-28 23:25:29 +0200

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,7 @@
+import "./App.css"
+import Stopwatch from "./components/stopwatch"
+function App() {
+  return <Stopwatch />
+}
+
+export default App
---
+++ src/components/stopwatch.tsx
@@ -0,0 +1,105 @@
+import React, { useState, useEffect } from "react"
+import "./stopwatch.css"
+const Stopwatch: React.FC = () => {
+  const [isRunning, setIsRunning] = useState<boolean>(false)
+  const [elapsedTime, setElapsedTime] = useState<number>(0)
+  const [lapTime, setLapTime] = useState<number>(0)
+  const [laps, setLaps] = useState<number[]>([])
+  const [counter, setCounter] = useState<number>(1)
+
+  interface lapItem {
+    lap: number
+    time: number
+  }
+
+  useEffect(() => {
+    let interval: number | undefined
+
+    if (isRunning) {
+      interval = setInterval(() => {
+        setElapsedTime((prevTime) => prevTime + 10)
+        setLapTime((prevTime) => prevTime + 10)
+      }, 10)
+    } else if (!isRunning && elapsedTime !== 0) {
+      clearInterval(interval)
+    }
+
+    return () => clearInterval(interval)
+  }, [isRunning])
+
+  const handleStartStop = () => {
+    setIsRunning(!isRunning)
+  }
+
+  const handleLap = () => {
+    if (isRunning) {
+      setLaps([lapTime, ...laps])
+      setLapTime(0)
+      setCounter((prev) => prev + 1)
+    } else {
+      setLaps([])
+      setLapTime(0)
+      setElapsedTime(0)
+      setCounter(1)
+    }
+  }
+
+  const formatTime = (time: number) => {
+    const minutes = Math.floor(time / 60000)
+    const seconds = Math.floor((time % 60000) / 1000)
+    const milliseconds = (time % 1000).toString().padStart(2, "0").substr(0, 2)
+
+    return `${minutes.toString().padStart(2, "0")}:${seconds
+      .toString()
+      .padStart(2, "0")}:${milliseconds}`
+  }
+
+  const startSopButtonClass = isRunning ? "stop-btn" : "start-btn"
+
+  return (
+    <div className="stopwatch-container">
+      <header>
+        <p>Stopwatch</p>
+      </header>
+      <div className="time-container">
+        <p className="time">{formatTime(elapsedTime)}</p>
+      </div>
+      <div className="button-container">
+        <button
+          className="gen-button lap-reset-btn"
+          onClick={handleLap}
+          disabled={!isRunning && elapsedTime === 0}
+        >
+          {isRunning || elapsedTime == 0 ? "Lap" : "Reset"}
+        </button>
+        <div className="dots">
+          <div className="dot"></div>
+          <div className="dot-empty"></div>
+        </div>
+        <button
+          className={`gen-button ${startSopButtonClass}`}
+          onClick={handleStartStop}
+        >
+          {isRunning ? "Stop" : "Start"}
+        </button>
+      </div>
+      <div className="laps-container">
+        <ul className="laps-table">
+          {(isRunning || elapsedTime > 0) && (
+            <li className="lap">{`Lap ${counter} ${formatTime(lapTime)}`}</li>
+          )}
+          {laps.map((lap, index) => (
+            <div className="laps">
+              <li className="lap" key={index}>
+                {`Lap ${index + 1} ${formatTime(lap)}`}
+              </li>
+            </div>
+          ))}
+        </ul>
+      </div>
+      <footer></footer>
+    </div>
+  )
+}
+
+export default Stopwatch
