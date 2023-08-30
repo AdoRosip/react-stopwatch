846ec4c0dd41adcd216468a76b83a3f83ea84814
Adrian Rosipal <adrian.rosipal98@gmail.com>
2023-08-30 20:04:29 +0200

    feat: styling and lap functionality done

--- src/components/stopwatch.tsx
+++ src/components/stopwatch.tsx
@@ -1,5 +1,11 @@
 import React, { useState, useEffect } from "react"
 import "./stopwatch.css"
+import alarmclock from "../assets/icons8-alarm-clock-30.png"
+import timer from "../assets/icons8-timer-50.png"
+import bedtime from "../assets/icons8-bed-time-64.png"
+import globe from "../assets/icons8-globe-64.png"
+import stopwatch from "../assets/stopwatch.png"
+
 const Stopwatch: React.FC = () => {
   const [isRunning, setIsRunning] = useState<boolean>(false)
   const [elapsedTime, setElapsedTime] = useState<number>(0)
@@ -57,51 +63,74 @@ const Stopwatch: React.FC = () => {
   const startSopButtonClass = isRunning ? "stop-btn" : "start-btn"
 
   return (
-    <div className="stopwatch-container">
-      <header>
-        <p>Stopwatch</p>
-      </header>
-      <div className="time-container">
-        <p className="time">{formatTime(elapsedTime)}</p>
-      </div>
-      <div className="button-container">
-        <button
-          className="gen-button lap-reset-btn"
-          onClick={handleLap}
-          disabled={!isRunning && elapsedTime === 0}
-        >
-          {isRunning || elapsedTime == 0 ? "Lap" : "Reset"}
-        </button>
-        <div className="dots">
-          <div className="dot"></div>
-          <div className="dot-empty"></div>
+    <body>
+      <div className="stopwatch-container">
+        <header>
+          <p>Stopwatch</p>
+        </header>
+        <div className="time-container">
+          <p className="time">{formatTime(elapsedTime)}</p>
         </div>
-        <button
-          className={`gen-button ${startSopButtonClass}`}
-          onClick={handleStartStop}
-        >
-          {isRunning ? "Stop" : "Start"}
-        </button>
+        <div className="button-container">
+          <button
+            className="gen-button lap-reset-btn"
+            onClick={handleLap}
+            disabled={!isRunning && elapsedTime === 0}
+          >
+            {isRunning || elapsedTime == 0 ? "Lap" : "Reset"}
+          </button>
+          <div className="dots">
+            <div className="dot"></div>
+            <div className="dot-empty"></div>
+          </div>
+          <button
+            className={`gen-button ${startSopButtonClass}`}
+            onClick={handleStartStop}
+          >
+            {isRunning ? "Stop" : "Start"}
+          </button>
+        </div>
+        <div className="laps-container">
+          <ul className="laps-table">
+            {(isRunning || elapsedTime > 0) && (
+              <li className="lap">
+                {" "}
+                <span>{`Lap ${counter}`}</span>
+                <span> {` ${formatTime(lapTime)}`}</span>
+              </li>
+            )}
+            {laps.map((lap, index) => (
+              <li className="lap" key={index}>
+                <span>{`Lap ${lap.lap}`}</span>
+                <span> {` ${formatTime(lap.time)}`}</span>
+              </li>
+            ))}
+          </ul>
+        </div>
+        <footer>
+          <div className="icon-set">
+            <img src={globe} alt="World Clock" />
+            <span>World Clock</span>
+          </div>
+          <div className="icon-set">
+            <img src={alarmclock} alt="Alarm" />
+            <span>Alarm</span>
+          </div>
+          <div className="icon-set">
+            <img src={bedtime} alt="Bedtime" />
+            <span>Bedtime</span>
+          </div>
+          <div className="icon-set">
+            <img src={stopwatch} alt="Stopwatch" />
+            <span>Stopwatch</span>
+          </div>
+          <div className="icon-set">
+            <img src={timer} alt="Timer" />
+            <span>Timer</span>
+          </div>
+        </footer>
       </div>
-      <div className="laps-container">
-        <ul className="laps-table">
-          {(isRunning || elapsedTime > 0) && (
-            <li className="lap">
-              {" "}
-              <span>{`Lap ${counter}`}</span>
-              <span> {` ${formatTime(lapTime)}`}</span>
-            </li>
-          )}
-          {laps.map((lap, index) => (
-            <li className="lap" key={index}>
-              <span>{`Lap ${lap.lap}`}</span>
-              <span> {` ${formatTime(lap.time)}`}</span>
-            </li>
-          ))}
-        </ul>
-      </div>
-      <footer></footer>
-    </div>
+    </body>
   )
 }
 
